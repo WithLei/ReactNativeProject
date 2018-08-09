f15ac00a919e32a016d63b9082b1e16e8dc961aa
WithLei <syunki@yeah.net>
2018-08-09 17:21:01 +0800

    8.9

--- TestProject/App.js
+++ TestProject/App.js
@@ -77,40 +77,249 @@
 //  }
 //}
 
+// import React, { Component } from 'react';
+// import { Text, View } from 'react-native';
+//
+// class Blink extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { showText: true };
+//
+//     // 每1000毫秒对showText状态做一次取反操作
+//     setInterval(() => {
+//       this.setState(previousState => {
+//         return { showText: !previousState.showText };
+//       });
+//     }, 1000);
+//   }
+//
+//   render() {
+//     // 根据当前showText的值决定是否显示text内容
+//     let display = this.state.showText ? this.props.text : ' ';
+//     return (
+//       <Text>{display}</Text>
+//     );
+//   }
+// }
+//
+// export default class BlinkApp extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Blink text='I love to blink' />
+//         <Blink text='Yes blinking is so great' />
+//         <Blink text='Why did they ever take this out of HTML' />
+//         <Blink text='Look at me look at me look at me' />
+//       </View>
+//     );
+//   }
+// }
+
+// import React, { Component } from 'react';
+// import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+//
+// export default class LotsOfStyles extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text style={styles.red}>just red</Text>
+//         <Text style={styles.bigblue}>just bigblue</Text>
+//         <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+//         <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   bigblue: {
+//     color: 'blue',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//   },
+//   red: {
+//     color: 'red',
+//   },
+// });
+
+// import React, { Component } from 'react';
+// import { AppRegistry, View } from 'react-native';
+//
+// export default class FixedDimensionsBasics extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
+
+// import React, { Component } from 'react';
+// import { AppRegistry, View } from 'react-native';
+//
+// export default class FlexDimensionsBasics extends Component {
+//   render() {
+//     return (
+//       // 试试去掉父View中的`flex: 1`。
+//       // 则父View不再具有尺寸，因此子组件也无法再撑开。
+//       // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+//       <View style={{height: 300}}>
+//         <View style={{flex: 100, backgroundColor: 'powderblue'}} />
+//         <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+//         <View style={{flex: 2, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
+
+
+// import React, { Component } from 'react';
+// import { AppRegistry, View } from 'react-native';
+//
+// export default class FlexDirectionBasics extends Component {
+//   render() {
+//     return (
+//       // 尝试把`flexDirection`改为`column`看看
+//       <View style={{flex: 1, flexDirection: 'row'}}>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// };
+
+// import React, { Component } from 'react';
+// import { AppRegistry, View } from 'react-native';
+//
+// export default class JustifyContentBasics extends Component {
+//   render() {
+//     return (
+//       // 尝试把`justifyContent`改为`center`看看
+//       // 尝试把`flexDirection`改为`row`看看
+//       <View style={{
+//         flex: 1,
+//         flexDirection: 'column',
+//         justifyContent: 'space-between',
+//       }}>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// };
+
+// import React, { Component } from 'react';
+// import { AppRegistry, View } from 'react-native';
+//
+// export default class AlignItemsBasics extends Component {
+//   render() {
+//     return (
+//       // 尝试把`alignItems`改为`flex-start`看看
+//       // 尝试把`justifyContent`改为`flex-end`看看
+//       // 尝试把`flexDirection`改为`row`看看
+//       <View style={{
+//         flex: 1,
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//       }}>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// };
+
+// import React, { Component } from 'react';
+// import { AppRegistry, Text, TextInput, View ,Button, Alert} from 'react-native';
+//
+// export default class PizzaTranslator extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {text: ''};
+//   }
+//
+//   render() {
+//     return (
+//       <View style={{padding: 10}}>
+//         <TextInput
+//           style={{height: 40}}
+//           placeholder="Type here to translate!"
+//           onChangeText={(text) => this.setState({text})}
+//         />
+//         <Button
+//           onPress={() => {
+//           Alert.alert("你点击了按钮！");}}
+//           title="点我！"
+//           />
+//         <Text style={{padding: 10, fontSize: 42}}>
+//           {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showText: true };
-
-    // 每1000毫秒对showText状态做一次取反操作
-    setInterval(() => {
-      this.setState(previousState => {
-        return { showText: !previousState.showText };
-      });
-    }, 1000);
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
   }
 
   render() {
-    // 根据当前showText的值决定是否显示text内容
-    let display = this.state.showText ? this.props.text : ' ';
     return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
-  render() {
-    return (
-      <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
