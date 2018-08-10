d2552b7e7aae679b35d98a69846ca0bfa89d0154
WithLei <syunki@yeah.net>
2018-08-10 16:30:22 +0800

    8.10

--- TestProject/App.js
+++ TestProject/App.js
@@ -266,60 +266,360 @@
 //   }
 // }
 
-import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+// import React, { Component } from 'react';
+// import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+//
+// export default class ButtonBasics extends Component {
+//   _onPressButton() {
+//     Alert.alert('You tapped the button!')
+//   }
+//
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.buttonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="Press Me"
+//           />
+//         </View>
+//         <View style={styles.buttonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="Press Me"
+//             color="#841584"
+//           />
+//         </View>
+//         <View style={styles.alternativeLayoutButtonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="This looks great!"
+//           />
+//           <Button
+//             onPress={this._onPressButton}
+//             title="OK!"
+//             color="#841584"
+//           />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//    flex: 1,
+//    justifyContent: 'center',
+//   },
+//   buttonContainer: {
+//     margin: 20
+//   },
+//   alternativeLayoutButtonContainer: {
+//     margin: 20,
+//     flexDirection: 'row',
+//     justifyContent: 'space-between'
+//   }
+// })
+//
+// // skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
 
-export default class ButtonBasics extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
+// import React, { Component } from 'react';
+// import { Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+//
+// export default class Touchables extends Component {
+//   _onPressButton() {
+//     Alert.alert('You tapped the button!')
+//   }
+//
+//   _onLongPressButton() {
+//     Alert.alert('You long-pressed the button!')
+//   }
+//
+//
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableHighlight</Text>
+//           </View>
+//         </TouchableHighlight>
+//         <TouchableOpacity onPress={this._onPressButton}>
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableOpacity</Text>
+//           </View>
+//         </TouchableOpacity>
+//         <TouchableNativeFeedback
+//             onPress={this._onPressButton}
+//             background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+//           </View>
+//         </TouchableNativeFeedback>
+//         <TouchableWithoutFeedback
+//             onPress={this._onPressButton}
+//             >
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+//           </View>
+//         </TouchableWithoutFeedback>
+//         <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>Touchable with Long Press</Text>
+//           </View>
+//         </TouchableHighlight>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: 60,
+//     alignItems: 'center'
+//   },
+//   button: {
+//     marginBottom: 30,
+//     width: 260,
+//     alignItems: 'center',
+//     backgroundColor: '#2196F3'
+//   },
+//   buttonText: {
+//     padding: 20,
+//     color: 'white'
+//   }
+// })
+
+// import React, { Component } from 'react';
+// import { ScrollView, Image, Text, StyleSheet} from 'react-native';
+//
+// export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+//   render() {
+//       return (
+//         <ScrollView>
+//           <Text style={{fontSize:24}}>Scroll me plz</Text>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Text style={{fontSize:24}}>If you like</Text>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Text style={{fontSize:24}}>Scrolling down</Text>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Text style={{fontSize:24}}>Whats the best</Text>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Text style={{fontSize:24}}>Framework around?</Text>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Image source={require('./img/user.jpg')} style={styles.img}/>
+//           <Text style={{fontSize:18}}>React Native</Text>
+//         </ScrollView>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   img: {
+//     width: 100,
+//     height: 100
+//   }
+// })
+
+// import React, { Component } from 'react';
+// import { FlatList, StyleSheet, Text, View } from 'react-native';
+//
+// export default class FlatListBasics extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <FlatList
+//           data={[
+//             {key: 'Devin'},
+//             {key: 'Jackson'},
+//             {key: 'James'},
+//             {key: 'Joel'},
+//             {key: 'John'},
+//             {key: 'Jillian'},
+//             {key: 'Jimmy'},
+//             {key: 'Julie'},
+//           ]}
+//           renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+//         />
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//    flex: 1,
+//    paddingTop: 22
+//   },
+//   item: {
+//     padding: 10,
+//     fontSize: 18,
+//     height: 44,
+//   },
+// })
+
+// import React, { Component } from 'react';
+// import { SectionList, StyleSheet, Text, View } from 'react-native';
+//
+// export default class SectionListBasics extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <SectionList
+//           sections={[
+//             {title: 'D', data: ['Devin']},
+//             {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+//           ]}
+//           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+//           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+//           keyExtractor={(item, index) => index}
+//         />
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//    flex: 1,
+//    paddingTop: 22
+//   },
+//   sectionHeader: {
+//     paddingTop: 2,
+//     paddingLeft: 10,
+//     paddingRight: 10,
+//     paddingBottom: 2,
+//     fontSize: 14,
+//     fontWeight: 'bold',
+//     backgroundColor: 'rgba(247,247,247,1.0)',
+//   },
+//   item: {
+//     padding: 10,
+//     fontSize: 18,
+//     height: 44,
+//   },
+// })
+
+import React, { Component } from "react";
+
+import { Image, FlatList, StyleSheet, Text, View } from "react-native";
+
+var REQUEST_URL =
+  "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
+
+export default class SampleAppMovies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false
+    };
+    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向会变为空
+    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then(response => response.json())
+      .then(responseData => {
+        // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+        this.setState({
+          data: this.state.data.concat(responseData.movies),
+          loaded: true
+        });
+      });
   }
 
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderMovie}
+        style={styles.list}
+      />
+    );
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#841584"
-          />
+        <Text>Loading movies...</Text>
+      </View>
+    );
+  }
+
+  renderMovie({ item }) {
+    // { item }是一种“解构”写法，请阅读ES2015语法的相关文档
+    // item也是FlatList中固定的参数名，请阅读FlatList的相关文档
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: item.posters.thumbnail }}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.year}>{item.year}</Text>
         </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
-   flex: 1,
-   justifyContent: 'center',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  buttonContainer: {
-    margin: 20
+  rightContainer: {
+    flex: 1
   },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: "center"
+  },
+  year: {
+    textAlign: "center"
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  },
+  list: {
+    paddingTop: 20,
+    backgroundColor: "#F5FCFF"
   }
-})
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
+});
