c53534d8d017c24bd7295c28be7e96ccf56163c4
WithLei <syunki@yeah.net>
2018-08-18 09:56:44 +0800

    8.18

--- TestProject/test.js
+++ TestProject/test.js
@@ -1,32 +1,6 @@
-'use strict';
-function asyncF(name){
-  return new Promise(function(resolve){
-    setTimeout(function(){
-      resolve('my name is ' + name);
-    });
-  });
-}
+'use strict'
 
-function* fn(){
-  console.log('test');
-  console.log('return' + (yield asyncF('leo')));
-}
-
-let gf = fn();
-
-function exec(gf, value){
-  console.log(gf + " " + value);
-  let result = gf.next(value);
-  if(!result.done){
-    if(result.value instanceof Promise){
-      result.value.then(function(v){
-        exec(gf,v);
-      });
-    }else{
-      exec(gf,result.value);
-    }
-  }
-
-}
-
-exec(gf);
+let st = new Set();
+st.add({name:'renly',age:20});
+st.add({name:'renly',age:20});
+console.log(st);
